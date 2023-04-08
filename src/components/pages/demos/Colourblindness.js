9bc83f59071a8afac9fcb42dad85c21a97f6bcd2
Ivan Chen <i28chen@uwaterloo.ca>
2023-04-08 11:51:27 -0400

    Added template for colourblindness page.

---
+++ src/components/pages/demos/Colourblindness.js
@@ -0,0 +1,27 @@
+import '../../../App.css'
+import React from 'react'
+
+function Colourblindness() {
+  return (
+    <>
+      <div className='standard-page'>
+        <h1 className='title'>Colourblindness</h1>
+        <p>
+          text here
+        </p>
+        <figure>
+          <img
+            src="/images/demos/motor-impairment/main-card.jpg"
+            alt="Elderly person with impaired movement in the hand."
+          />
+          <figcaption>caption</figcaption>
+        </figure>
+        <p>
+          text here
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Colourblindness
